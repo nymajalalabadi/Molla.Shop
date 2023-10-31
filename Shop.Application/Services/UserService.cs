9e2c1222a79467c1a6fd378179a7bb5a1254130a
nyma jalalabadi <nima.jalalabadii@gmail.com>
2023-10-31 17:42:33 +0330

    Add IUser service and IUser repository

---
+++ Shop.Application/Services/UserService.cs
@@ -0,0 +1,24 @@
+﻿using Shop.Application.Interfaces;
+using Shop.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Application.Services
+{
+    public class UserService : IUserService
+    {
+        #region constractor
+
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        #endregion
+    }
+}
---
+++ Shop.Infra.Data/Repositories/UserRepository.cs
@@ -0,0 +1,24 @@
+﻿using Shop.Domain.Interfaces;
+using Shop.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Infra.Data.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        #region constractore
+
+        private readonly ShopDbContext _context;
+
+        public UserRepository(ShopDbContext context)
+        {
+               _context = context;
+        }
+
+        #endregion
+    }
+}
