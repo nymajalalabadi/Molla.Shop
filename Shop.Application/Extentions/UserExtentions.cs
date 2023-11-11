4f9a27a8f47a8a364e027d10d9dcacaca7798c56
nyma jalalabadi <nima.jalalabadii@gmail.com>
2023-11-11 17:00:27 +0330

    Add Extention Method For User

---
+++ Shop.Application/Extentions/UserExtentions.cs
@@ -0,0 +1,23 @@
+﻿using Shop.Domain.Models.Account;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Application.Extentions
+{
+    public static class UserExtentions
+    {
+        public static string GetUserName(this User user)
+        {
+            //if (!string.IsNullOrWhiteSpace(user.FirstName) && string.IsNullOrWhiteSpace(user.LastName))
+            //{
+            //    return $"{user.FirstName} {user.LastName}";
+            //}
+            //return user.PhoneNumber ;
+
+            return $"{user.FirstName} {user.LastName}";
+        }
+    }
+}
