3b9e1327b5ce81c40bed73b61b1ae657289e0055
nyma jalalabadi <nima.jalalabadii@gmail.com>
2023-11-13 17:32:35 +0330

    Add Utils For Shop

---
+++ Shop.Application/Utils/PathExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Application.Utils
+{
+    public static class PathExtensions
+    {
+        #region user avatar
+
+        public static string UserAvatarOrgin = "/img/userAvatar/orgin/";
+        public static string UserAvatarOrginServer = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/userAvatar/orgin/");
+
+        public static string UserAvatarThumb = "/img/userAvatar/thumb/";
+        public static string UserAvatarThumbServer = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/img/userAvatar/thumb/");
+
+        #endregion
+    }
+}
