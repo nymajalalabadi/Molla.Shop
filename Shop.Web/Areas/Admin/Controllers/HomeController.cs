d467a30217912bf7ea1d6d894f88043eceb1ebbe
nyma jalalabadi <nima.jalalabadii@gmail.com>
2023-11-20 18:28:50 +0330

    Transfer Admin Panel

---
+++ Shop.Web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Shop.Web.Areas.Admin.Controllers
+{
+    public class HomeController : AdminBaseController
+    {
+        #region dashboard
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #endregion
+
+    }
+}
