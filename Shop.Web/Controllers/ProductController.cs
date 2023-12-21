a77e036cfa8384dda5f6b86f83405028e49a028f
nyma jalalabadi <nima.jalalabadii@gmail.com>
2023-12-21 10:35:18 +0330

    Add CreateProductComment For Controll

--- Shop.Web/Controllers/ProductController.cs
+++ Shop.Web/Controllers/ProductController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Shop.Application.Interfaces;
 using Shop.Domain.ViewModels.Admin.Products;
+using Shop.Domain.ViewModels.Site.Products;
+using Shop.Web.Extentions;
 
 namespace Shop.Web.Controllers
 {
@@ -45,6 +47,38 @@ namespace Shop.Web.Controllers
             }
 
             return View(product);
+        }
+
+        #endregion
+
+        #region create-product-comment
+
+        [HttpPost("add-comment"),ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateProductComment(CreateProductCommentViewModel productComment)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _productService.CreateProductComment(productComment, User.GetUserId());
+
+                switch (result)
+                {
+                    case CreateProductCommentResult.CheckUser:
+                        TempData[WarningMessage] = "کاربر مورد نظر یافت نشد";
+                        break;
+
+                    case CreateProductCommentResult.CheckProduct:
+                        TempData[WarningMessage] = "محصولی یافت نشد";
+                        break;
+
+                    case CreateProductCommentResult.Success:
+                        TempData[SuccessMessage] = "نظر شما با موفقیت ثبت شد";
+
+                        return RedirectToAction("ProductDetail", new { productId = productComment.ProductId });
+                }
+            }
+
+            TempData[ErrorMessage] = "لطفا نظر خود را وارد نمایید";
+            return RedirectToAction("ProductDetail", new { productId = productComment.ProductId });
         }
 
         #endregion
