76775a9f0dbde3f0f6012e25a425bea4e50a281e
nyma jalalabadi <nima.jalalabadii@gmail.com>
2024-01-01 21:21:34 +0330

    Add OrderController for Admin

---
+++ Shop.Web/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shop.Application.Interfaces;
+using Shop.Domain.ViewModels.Admin.Orders;
+
+namespace Shop.Web.Areas.Admin.Controllers
+{
+    public class OrderController : AdminBaseController
+    {
+        #region constractor
+
+        private readonly IOrderService _orderService;
+
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        #endregion
+
+        #region filter-order
+
+        public async Task<IActionResult> FilterOrder(FilterOrdersViewModel filter)
+        {
+            return View(await _orderService.filterOrders(filter));
+        }
+
+        #endregion
+    }
+}
